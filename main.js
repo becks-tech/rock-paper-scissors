3492ca6d9603b485810122b6a2dff529ea3a16cf
Rebecca <techybecky21@gmail.com>
2023-04-29 19:20:47 +0100

    computer choice function works

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 console.log('hello world!')
+
+function getComputerChoice(){
+    let computerSelection = Math.floor(Math.random()* 3)
+    return computerSelection
+}
+console.log(getComputerChoice())
