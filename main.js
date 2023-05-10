5195a16e611f41c59ee77618de40403ad5931d4e
Rebecca <techybecky21@gmail.com>
2023-05-10 22:13:44 +0100

    changes made to aid styling

--- main.js
+++ main.js
@@ -1,34 +1,32 @@
 function game(){
     const buttons = document.querySelectorAll('input');
-    const message = document.querySelector('.message')
-    const scoreMsg = document.querySelector('.score')
     let playerScore = 0
     let computerScore = 0
     
     function getComputerChoice(){
         let computerSelection = Math.floor(Math.random()* 3)
         if (computerSelection === 0){
-            return "rock"
+            return "Rock"
           } else if (computerSelection === 1){
-            return "paper"
+            return "Paper"
           }
           else if (computerSelection === 2){
-            return "scissors"
+            return "Scissors"
           } 
         return computerSelection;
     }
  
     function score(){
-        let score = ""
+        let score = "Rock Paper Scissors"
         if(playerScore === 5){
             score = "You win! Your score is " + playerScore +". Reload the page to play again.";
-            buttons.disabled = true; 
         } else if(
             computerScore === 5){
             score = "You lose! Computer's score is " + computerScore + ". Reload the page to play again.";
-            buttons.disabled = true; 
         }
-        document.querySelector('.score').textContent = score
+        
+        document.querySelector('.result').textContent = score
+        
     }
     
     
@@ -39,19 +37,19 @@ function game(){
         if (playerSelection === computerSelection){
             result = "It's a tie!";
             
-        } else if (playerSelection === 'rock' && computerSelection === 'scissors' || 
-        playerSelection === 'paper' && computerSelection === 'rock' ||
-        playerSelection === 'scissors' && computerSelection === 'paper'){
+        } else if (playerSelection === 'Rock' && computerSelection === 'Scissors' || 
+        playerSelection === 'Paper' && computerSelection === 'Rock' ||
+        playerSelection === 'Scissors' && computerSelection === 'Paper'){
             playerScore+=1
             document.querySelector('.player-score').textContent = playerScore
-            result = 'You win! ' + playerSelection + ' beats ' + computerSelection + '. Your score currently is: ' + playerScore;
+            result = playerSelection + ' beats ' + computerSelection;
         }
-        else if (computerSelection === 'rock' && playerSelection === 'scissors' ||
-        computerSelection === 'paper' && playerSelection == 'rock' ||
-        computerSelection === 'scissors' && playerSelection === 'paper'){
+        else if (computerSelection === 'Rock' && playerSelection === 'Scissors' ||
+        computerSelection === 'Paper' && playerSelection == 'Rock' ||
+        computerSelection === 'Scissors' && playerSelection === 'Paper'){
             computerScore+=1
             document.querySelector('.computer-score').textContent = computerScore
-            result = 'Computer wins! ' + computerSelection + ' beats ' + playerSelection + ". Computer's score currently is: " + computerScore;
+            result = computerSelection + ' beats ' + playerSelection;
             
         }
         score()
